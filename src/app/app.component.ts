4ed15028c3f0c528fc8c2c8d9b13e0b64ceb06a2
Pablo Morelli <pablojm21@hotmail.com>
2023-07-17 10:08:05 -0300

    se añadio angular material y se completaron los puntos B y C del práctico.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,51 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
-  title = 'ejemplo';
+  
+  articulos:articulo[] = [
+    {
+      nombre: 'Bici',
+      costo: '3000'
+    },
+    {
+      nombre: 'Smart Tv',
+      costo: '2500'
+    },
+    {
+      nombre: 'Libro',
+      costo: '320'
+    },
+    {
+      nombre: 'Celular',
+      costo: '1000'
+    },
+    {
+      nombre: 'Laptop',
+      costo: '2199'
+    },
+    {
+      nombre: 'Teclado',
+      costo: '2000'
+    },
+    {
+      nombre: 'Audifonos',
+      costo: '1700'
+    },
+  ]
+  mostrar = true;
+
+  alternaLista() {
+    this.mostrar = !this.mostrar;
+  }
+
 }
+
+type articulo = {nombre:string; costo:string};
